b60cde1015a04fb29c02c7420bf6037fb35f7b18
Турсунов Баходур <tursunovb18@gmail.com>
2024-08-09 16:45:08 +0500

    здесь было использован модульный подход

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component } from "@angular/core";
+      
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: "my-app",
+    template: `<label>Введите имя:</label>
+                 <input [(ngModel)]="name" placeholder="name">
+                 <h1>Добро пожаловать {{name}}!</h1>`
 })
-export class AppComponent {
-  title = 'LibraryClient';
+export class AppComponent { 
+    name= '';
 }
