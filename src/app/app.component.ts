64ec9fcfe0028c7539f2e4030ffcbcc479b34f09
Турсунов Баходур <tursunovb18@gmail.com>
2024-08-12 09:29:49 +0500

    if else constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,16 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: "my-app",
-  standalone: false,
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <div>
+      <input [(ngModel)]="age" min="1" type="number"/>
+      <h2>@if (age > 0 && age < 110) {
+        Your age is {{ age }}
+      } @else {
+        age is undefined
+      }</h2>
+    </div>`
 })
 export class AppComponent {
+  age: number | undefined = undefined;
 }
